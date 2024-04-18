d057c72a0df561b778ecd9654c370bcd1fe6ad7e
Sanskaari <ankitmrmishra1118@gmail.com>
2024-04-19 00:47:42 +0530

    pass stage

--- app/main.js
+++ app/main.js
@@ -21,14 +21,14 @@ const server = net.createServer((socket) => {
      if (path === "/") {
        socket.write("HTTP/1.1 200 OK\r\n\r\n");
      }
-     else if(path === "/echo/") {
-            let ans = "";
-            ans += "HTTP/1.1 200 OK\r\n\r\n";
-            ans += "Content-Type: text/plainr\n\r\n"; 
-            ans+= `content.length: \r\n ${stringpassed.length}\n\r\n`;
-            ans += `\n\r\n${stringpassed}\n\r\n`;
-            socket.write(ans);
-    }else {
+     else if (path.startsWith("/echo/")) {
+       let ans = "";
+       ans += "HTTP/1.1 200 OK\r\n\r\n";
+       ans += "Content-Type: text/plainr\n\r\n";
+       ans += `content.length: \r\n ${stringpassed.length}\n\r\n`;
+       ans += `\n\r\n${stringpassed}\n\r\n`;
+       socket.write(ans);
+     } else {
        socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
      }
      socket.end();
